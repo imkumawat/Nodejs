970f9ba4436771e62e4677236c50034168c1da1c
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-12-20 15:36:36 +0530

    added and updated code

--- 02 File System/#3 Reading file in SyncMode.js
+++ 02 File System/#3 Reading file in SyncMode.js
@@ -2,10 +2,12 @@
 const fs = require("fs");
 
 //reading file in sync mode
-const buf_data = fs.readFileSync("01 WriteFile.txt");
+const buf_data = fs.readFileSync("01 ReadWriteFile.txt");
 
 //printing buffer data
 console.log(buf_data);
+
+//to have direct string and no buffer data specify file encoding "utf8"
 
 //converting buffer data into string data
 org_data = buf_data.toString();
